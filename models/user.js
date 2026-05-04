961fe3ce8c7389997f1a0ed3cf9284361241ec00
Raj Verma <rajbksc421@gmail.com>
2026-05-04 09:34:47 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -30,17 +30,8 @@ const userSchema = new mongoose.Schema({
     enum: ['Asia', 'Europe', 'Africa', 'North America', 'South America', 'Australia', 'Antarctica']
   }
 }, {
-  timestamps: true
 });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
 
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
 
 module.exports = mongoose.model('User', userSchema);
