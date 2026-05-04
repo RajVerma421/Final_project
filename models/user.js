b7bff88919c6504cfc78fbbe5ee1e352e7061001
Raj Verma <rajbksc421@gmail.com>
2026-05-04 09:46:30 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,37 +1,11 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, 'Username is required'],
-    unique: true,
-    trim: true,
-    minlength: [3, 'Username must be at least 3 characters']
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [6, 'Password must be at least 6 characters']
-  },
-  nationality: {
-    type: String,
-    required: [true, 'Nationality is required'],
-    trim: true
-  },
-  travelStyle: {
-    type: String,
-    required: [true, 'Travel style is required'],
-    enum: ['Adventure', 'Relaxation', 'Cultural', 'Business', 'Backpacking', 'Luxury']
-  },
-  favoriteContinent: {
-    type: String,
-    required: [true, 'Favorite continent is required'],
-    enum: ['Asia', 'Europe', 'Africa', 'North America', 'South America', 'Australia', 'Antarctica']
-  }
-}, {
+  username: String,
+  password: String,
+  nationality: String,
+  travelStyle: String,
+  favoriteContinent: String
 });
-
-
 
 module.exports = mongoose.model('User', userSchema);
